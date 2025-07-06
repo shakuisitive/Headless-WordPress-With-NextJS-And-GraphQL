9cc9957109ec8d348bba95c6f2cd358d5823dd8c
Shakir <shakirkhan72727@gmail.com>
2025-07-06 17:45:20 +0500

    generalise tailwind class

--- src/components/ButtonLink/ButtonLink.js
+++ src/components/ButtonLink/ButtonLink.js
@@ -2,10 +2,7 @@ import Link from "next/link";
 
 function ButtonLink({ destination, label }) {
   return (
-    <Link
-      href={destination}
-      className="bg-pink-500 hover:bg-pink-700 inline-block my-2 px-4 py-2 uppercase rounded-md cursor-pointer text-white font-bold"
-    >
+    <Link href={destination} className="btn">
       {label}
     </Link>
   );
